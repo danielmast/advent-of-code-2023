d0837e25cf8c2741049c2e328a71b59273db5ad4
Daniel Mast <danielmast92@gmail.com>
2023-12-01 07:13:13 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent-of-code-2023
 Advent of Code 2023 Solutions
+
+Link to my 2021 solutions:
+https://github.com/danielmast/advent-of-code-2021/tree/main
---
+++ util/util.py
@@ -0,0 +1,9 @@
+from aocd import get_data
+from aocd import submit
+
+# Get input
+data = get_data(day=1, year=2021)
+
+# Submit output
+submit(1759, part="a", day=1, year=2021)
+print('done')
